a80179198841a6aa68dbc97b2aea46d7c279bf80
Hamza Farhan <thehamza96@gmail.com>
2023-07-20 18:56:10 +0500

    format

--- vid_chains/utils.py
+++ vid_chains/utils.py
@@ -7,6 +7,7 @@ __all__ = ['load_obj_model', 'detect_objects', 'centroid', 'list_centroids', 'in
 from .imports import *
 import math
 
+
 # %% ../nbs/00_utils.ipynb 4
 def load_obj_model(name="yolov8n.pt"):
     return YOLO(name)
@@ -17,34 +18,34 @@ def detect_objects(model, img):
     return [{"boxes": r.boxes.data.detach().cpu().tolist()} for r in res]
 
 
-def centroid (l):
+def centroid(l):
     t = []
-    cx = (l[0]+l[2])/2.0
-    cy = (l[1]+l[3])/2.0
+    cx = (l[0] + l[2]) / 2.0
+    cy = (l[1] + l[3]) / 2.0
     t.append(cx)
     t.append(cy)
     return t
 
-def list_centroids (objects):
-    c=[]
-    for i in range(0,11):
-      l=[]
-      for j in range(0,4):
-        l.append(objects[0].get('boxes')[i][j])
-      centre = centroid(l)
-      c.append(centre)
+
+def list_centroids(objects):
+    c = []
+    for i in range(0, 11):
+        l = []
+        for j in range(0, 4):
+            l.append(objects[0].get("boxes")[i][j])
+        centre = centroid(l)
+        c.append(centre)
     return c
 
 
 def inter_dist(objects):
-   
     c = list_centroids(objects)
     dis = []
     st = []
-    for i in range(0,11):
-        for j in range(i+1,11):
-            #st.append("Distance b/w object "+str(i)+" and object "+str(j))
-            #st.append("D("+str(i)+","+str(j)+")")
-            dis.append(math.dist(c[i],c[j]))
-    #return st,dis
+    for i in range(0, 11):
+        for j in range(i + 1, 11):
+            # st.append("Distance b/w object "+str(i)+" and object "+str(j))
+            # st.append("D("+str(i)+","+str(j)+")")
+            dis.append(math.dist(c[i], c[j]))
+    # return st,dis
     return dis
