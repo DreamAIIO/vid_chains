b0e1b87dbb925c22c40acfcfe344715889db3645
RafayF1 <rafay.farhan@dreamai.io>
2023-07-26 01:16:30 +0500

    Distances function updated, width function added

--- vid_chains/_modidx.py
+++ vid_chains/_modidx.py
@@ -9,6 +9,8 @@ d = { 'settings': { 'branch': 'main',
             'vid_chains.imports': {},
             'vid_chains.utils': { 'vid_chains.utils.centroid': ('utils.html#centroid', 'vid_chains/utils.py'),
                                   'vid_chains.utils.detect_objects': ('utils.html#detect_objects', 'vid_chains/utils.py'),
+                                  'vid_chains.utils.get_width': ('utils.html#get_width', 'vid_chains/utils.py'),
                                   'vid_chains.utils.inter_dist': ('utils.html#inter_dist', 'vid_chains/utils.py'),
                                   'vid_chains.utils.list_centroids': ('utils.html#list_centroids', 'vid_chains/utils.py'),
+                                  'vid_chains.utils.list_widths': ('utils.html#list_widths', 'vid_chains/utils.py'),
                                   'vid_chains.utils.load_obj_model': ('utils.html#load_obj_model', 'vid_chains/utils.py')}}}
